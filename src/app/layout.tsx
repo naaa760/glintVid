d0a74fdcb8b3abe992590d79fba612ff7b3eb2db
nehhhhha6 <n0370525@gmail.com>
2024-11-26 12:05:49 +0530

    update

---
+++ src/app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import { Manrope } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+import "./globals.css";
+import { ThemeProvider } from "next-themes";
+
+const manrope = Manrope({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "GlintVid",
+  description: "Share AI Powered videos with your friends.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${manrope.className} bg-[#171717]`}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
